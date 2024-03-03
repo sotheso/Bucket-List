2d4263d83b851b9366889f04bd9fcbfdd6d071b9
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-03-02 23:37:54 -0800

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Sothesom on 12/12/1402.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
